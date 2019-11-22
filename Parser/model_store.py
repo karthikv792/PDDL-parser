fabf399bb4d2f3d6bb2ae0522e9e42d3157cbb34
kvalmeek <karthikv792@gmail.com>
2019-11-21 17:02:45 -0700

    Adding UI - 1

--- Parser/model_store.py
+++ Parser/model_store.py
@@ -1,23 +1,38 @@
 import pymysql
 pymysql.install_as_MySQLdb()
 import MySQLdb
-import os
+# import os
 from parser_new import *
 from writer_new import *
 from constants import *
-from tarski.syntax import VariableBinding, Variable
-# db = MySQLdb.connect('localhost','root','yochan','radar')
-# cur = db.cursor()
-# cur.execute("CREATE TABLE modelFile(model VARCHAR(1000))")
-model1 = parse_model('domain.pddl','problem.pddl')
 from json import dumps,loads
-j = dumps(model1)
-model1=loads(j)
-# print(model1.keys())
-# sql = "INSERT INTO modelFile(model) VALUES (%s)"
-# cur.execute(sql, model1)
-# cur.commit()
+
+def add_model(cur,model):
+    j = dumps(model)
+    cur.execute("DROP TABLE modelFile")
+    cur.execute("CREATE TABLE modelFile(model TEXT)")
+    sql = "INSERT INTO modelFile(model) VALUES (%s)"
+    cur.execute(sql, j)
+    db.commit()
+
+def retrieve_model(cur):
+    cur.execute("SELECT * from modelFile")
+    model = cur.fetchall()
+    j = loads(model[0][0])
+    return j
+
+# model1 = parse_model('domain.pddl','problem.pddl')
+
+db = MySQLdb.connect('localhost','root','yochan','radar')
+cur = db.cursor()
+model1 = retrieve_model(cur)
+print(model1)
+# add_model(cur,j)
 model11 = ModelWriter(model1)
 model11.write_files('write_domain1.pddl','write_problem1.pddl')
-#
+
+
+
+
+
 
